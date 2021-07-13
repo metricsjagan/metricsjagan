7b3d4a1d8b728cee41505254f2da82f012329d8c
metricsjagan <78196078+metricsjagan@users.noreply.github.com>
2021-07-13 14:38:03 +0530

    Create ahb_if.sv

---
+++ ahb_if.sv
@@ -0,0 +1,14 @@
+//This is AHB interface file 
+//project : AHB VIP 
+interface ahb_if (input hclk,Input hreset_n)
+  logic [31:0] haddr;
+  logic        hmastlock;
+  logic [2:0]  hsize;
+  logic [10]  htrans;
+  logic [31:0] hwdata;
+  logic        hwrite;
+  logic [31:0] hrata;
+  logic        hready;
+  logic        hresp;
+endinterface 
+  
