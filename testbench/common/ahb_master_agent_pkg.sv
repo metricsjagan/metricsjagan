141995c698420644a63784ef68bea7c65692d907
metricsjagan <78196078+metricsjagan@users.noreply.github.com>
2021-07-28 17:01:05 +0530

    Create ahb_master_agent_pkg.sv
    initial draft

---
+++ testbench/common/ahb_master_agent_pkg.sv
@@ -0,0 +1,19 @@
+package ahb_master_agent_pkg;
+
+import uvm_pkg::*;
+  //`include "uvm_macros.svh"
+
+
+
+  `include "ahb_master_defines.sv"
+  `include "ahb_master_types.sv"
+  `include "ahb_master_transaction_c.sv"
+
+  `include "ahb_master_sequencer.sv"
+  `include "ahb_master_driver.sv"
+  `include "ahb_master_monitor.sv"
+  `include "ahb_master_agent_config.sv"
+  `include "ahb_master_agent.sv"
+
+
+endpackage: ahb_mater_agent_pkg
