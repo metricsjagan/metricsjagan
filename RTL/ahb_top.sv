1a01dea0b0da469df3707cdfb40872bf8505d86f
metricsjagan <78196078+metricsjagan@users.noreply.github.com>
2021-07-29 16:13:24 +0530

    Create ahb_top.sv
    Inital draft

---
+++ RTL/ahb_top.sv
@@ -0,0 +1,21 @@
+mudule ahb_top(
+  input hclk;
+  input [31:0] haddr;
+  input        hmastlock;
+  inout [2:0]  hsize;
+  input [1:0]  htrans;
+  input [31:0] hwdata;
+  input        hwrite;
+  input [31:0] hrata;
+  output        hready;
+  output        hresp;
+);
+  
+
+always (posedle hclk) begin
+end
+  
+  
+  
+  
+  endmodule 
