1d81fc21007c9abea622b426e4621cb995632c46
metricsjagan <78196078+metricsjagan@users.noreply.github.com>
2021-08-04 14:42:26 +0530

    Create tb_top.v
    Initial draft

---
+++ testbench/TB/tb_top.v
@@ -0,0 +1,19 @@
+module tb_top();
+  
+  input [31:0] haddr;
+  input        hmastlock;
+  input [2:0]  hsize;
+  input [1:0]  htrans;
+  input [31:0] hwdata;
+  input        hwrite;
+  output [31:0] hrata;
+  output        hready;
+  output       hresp;
+  
+  
+  
+  
+  
+  
+  
+endmodule 
