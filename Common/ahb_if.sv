d60acebdbb41916c446bcc1c3574b20020ccda40
metricsjagan <78196078+metricsjagan@users.noreply.github.com>
2021-08-24 19:30:51 +0530

    Update ahb_if.sv
    initial draft

--- Common/ahb_if.sv
+++ Common/ahb_if.sv
@@ -1,1 +1,13 @@
-
+//This is AHB interface file 
+//project : AHB VIP 
+interface ahb_if (input hclk,Input hreset_n)
+  logic [31:0] haddr;
+  logic        hmastlock;
+  logic [2:0]  hsize;
+  logic [1:0]  htrans;
+  logic [31:0] hwdata;
+  logic        hwrite;
+  logic [31:0] hrata;
+  logic        hready;
+  logic        hresp;
+endinterface 
